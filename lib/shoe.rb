96c105e4648a8c1087d841f8c7f9f30493170f99
meili <meiliham1@gmail.com>
2021-02-23 00:52:46 -0500

    commit tree f4a6ddf1e14c8b14308c76ad73369b081e0dd3e1

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,26 @@
+require "pry"
+
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :color, :material, :condition
+  attr_reader :brand, :size
+
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+     if !BRANDS.include?(brand) #checks conditional, if !BRANDS(Bang means does not, can be used this way in front of the method called) does not include the brands
+      BRANDS << brand  #then push the brands into the empty array
+  
+  #The above of .include, basically checks to see if each brand is in the array. If not, it adds it. 
+  #If it already is in there, IT WILL Not ADD IT AGAIN, thus keeping it at 3 brands.
+      end
   end
+
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+
 
 end
